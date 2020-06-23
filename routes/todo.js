3e1d0720ebaa4249e017d658997b9d25b81337fa
Chopper356 <vedmedenko.nikita345@gmail.com>
2020-06-23 23:27:56 +0300

    Connection to database and create todo model

---
+++ routes/todo.js
@@ -0,0 +1,25 @@
+const {Router} = require("express");
+const Todo = require("../models/todo");
+const router = Router();
+
+// Получение списка задач
+router.get("/", (req, res) => {
+	res.json({a: 1});
+});
+
+//Создание новой задачи
+router.post("/", (req, res) => {
+
+});
+
+//Изменение задачи
+router.put("/:id", (req, res) => {
+
+});
+
+//Удаление задачи
+router.delete("/:id", (req, res) => {
+
+});
+
+module.exports = router;
